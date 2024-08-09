b3f1bc2fdb76ad0f54ffd0d234ed053cad86092f
Morgan Packard <packard25m@ncssm.edu>
2024-08-08 21:44:35 -0400

    Began the initial steps towards making file work

--- main.cpp
+++ main.cpp
@@ -7,24 +7,19 @@ class PointMass {
         double x, y, vx, vy;
     public:
         void setValues (double, double, double, double);
-        double getAcceleration (double, double, int, bool); //true = x false = y
+        // double getAcceleration (double, double, int, bool); //true = x false = y
         double getXValue () {return x;};
         double getYValue () {return y;};
-        double getVXValue () {return vx;};
-        double getVYValue () {return vy;};
+        // double getVXValue () {return vx;};
+        // double getVYValue () {return vy;};
 };
 
-/*
-n bodies
-F=GMm/
-
-*/
-// void PointMass::setValues (double xin, double yin, double vxin, double vyin) {
-//     x = xin;
-//     y = yin;
-//     vx = vxin;
-//     vy = vyin;
-// }
+void PointMass::setValues (double xin, double yin, double vxin, double vyin) {
+    x = xin;
+    y = yin;
+    vx = vxin;
+    vy = vyin;
+}
 
 // double PointMass::getAcceleration (double xTotal, double yTotal, int numberOfBodies, bool xTrueYFalse) {
 //     double xComMinusX = (xTotal-x)/(numberOfBodies-1)-x;
@@ -42,32 +37,39 @@ F=GMm/
 // each particle must affect individually. Then the acceleration will be summed.
 // 2D array of doubles? x, y, number of bodies, true false
 // 
+
+
+/*
+n bodies
+for each body, this equation must be summed for every other body F=GMm/r^2
+*/
+
 int main(){
-
-    // int endTime, deltaT, numberOfBodies;
-
-    std::cout << "Please enter the deltaT: ";
-    // std::cin >> deltaT;
-    std::cout << "Please enter the end time: ";
-    // std::cin >> endTime;
-    std::cout << "Please enter the number of bodies: ";
-    // std::cin >> numberOfBodies;
-
-    // std::vector<PointMass> masses (numberOfBodies);
-    // for (PointMass &body : masses)
-    // {
-    //     double x, y, vx, vy;
-    //     std::cout << "Please enter the x position of body: ";
-    //     std::cin >> x;
-    //     std::cout << "Please enter the y position of body: ";
-    //     std::cin >> y;
-    //     std::cout << "Please enter the x velocity of body: ";
-    //     std::cin >> vx;
-    //     std::cout << "Please enter the y velocity of body: ";
-    //     std::cin >> vy;
+    double deltaT = 1;
+    double endTime = 1000;
+    int numberOfBodies = 3;    
+    
+    double x, y, vx, vy;
+    std::vector<PointMass> masses (numberOfBodies);
+    for (PointMass &body : masses)
+    {
+        std::cout << "Please enter the x position of body: ";
+        std::cin >> x;
+        std::cout << "Please enter the y position of body: ";
+        std::cin >> y;
+        std::cout << "Please enter the x velocity of body: ";
+        std::cin >> vx;
+        std::cout << "Please enter the y velocity of body: ";
+        std::cin >> vy;
         
-    //     body.setValues(x, y, vx, vy);
-    // }
+        body.setValues(x, y, vx, vy);
+    }
+
+    for (PointMass &body : masses)
+    {
+        std::cout << body.getXValue() << body.getYValue() << std::endl;
+
+    }
 
     // for (int t = 0; t < endTime; t += deltaT)
     // {
